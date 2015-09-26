88de0c0c8ba7d3c7d2afb9d3068cd9c8ca1c771a
zach <fenfir@gmail.com>
2015-09-26 12:28:41 +0200

    Add function for showing current position on the map.

--- script/app.js
+++ script/app.js
@@ -3,16 +3,40 @@ var platform = new H.service.Platform({
   'app_code': 'ja49DO9r9lVmZaMjVsneEA'
 });
 
-function initializeMap() {
-// Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
+var currentLocationMarker;
+var map;
 
-// Instantiate (and display) a map object:
-var map = new H.Map(
-    document.getElementById('mapContainer'),
-    defaultLayers.normal.map,
-    {
-      zoom: 10,
-      center: { lat: 52.5, lng: 13.4 }
+function initializeMap() {
+    // Obtain the default map types from the platform object:
+    var defaultLayers = platform.createDefaultLayers();
+
+    // Instantiate (and display) a map object:
+    map = new H.Map(
+	document.getElementById('mapContainer'),
+	defaultLayers.normal.map,
+	{
+	    zoom: 10,
+	    center: { lat: 52.5, lng: 13.4 }
     });
+}
+
+function getGPSLocation() {
+    if(navigator.geolocation) {
+	console.log("Getting position");
+	navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+	alert("Browser does not support geolocation.");
+    }
+}
+
+function showPosition(position) {
+    if(map !== null) {	
+	currentLocationMarker = new H.map.Marker({
+	    lat: position.coords.latitude,
+	    lng: position.coords.longitude
+	});
+	
+	map.addObject(currentLocationMarker);
+    }
 }
