27180672187fbb27f3b48d559303a9c24a555505
zach <fenfir@gmail.com>
2015-09-27 11:24:04 +0200

    Default to current location. fix null error on reload

--- script/map.js
+++ script/map.js
@@ -27,90 +27,97 @@ Map.prototype.init = function(latitude, longitude) {
         center: { lat: latitude, lng: longitude }
       });
 
-      var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));      
+      var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
 
-      this.currentLocationMarker = new H.map.Marker({ lat: latitude, lng: longitude });
-      this.map.addObject(this.currentLocationMarker);
-      this.map.setZoom(15, true);
+      this.currentLocationMarker = new H.map.Marker({ lat: latitude, lng: longitude },
+        {data: {
+          x: 0,
+          y: 0,
+          size: 10,
+          color: "black",
+          text: "I'm a label"
+        }});
+        this.map.addObject(this.currentLocationMarker);
+        this.map.setZoom(15, true);
 
-      this.isInit = true;
+        this.isInit = true;
+      }
     }
-  }
 
-  Map.prototype.load = function(endLocation) {
-    var mapObject = this;
-    var endCoordinates = this.geocode(endLocation, function(result) {
-      loadSuccess(mapObject, result)
-    });
-  }
-
-  function loadSuccess(mapObject, result) {
-    if(result.Response.View.length > 0 && result.Response.View[0].Result.length > 0) {
-      var locations = result.Response.View[0].Result;
-      // TODO: handle multiple results
-      var coords = locations[0].Location.DisplayPosition;
-      console.log("Destination: " + coords.Longitude + ", " + coords.Latitude);
-      mapObject.calculateRoute(mapObject.gpsObject.getLocation(), { lat: coords.Latitude, lng: coords.Longitude }, function(result) {
-        routingSuccess(mapObject, result);
+    Map.prototype.load = function(endLocation) {
+      var mapObject = this;
+      var endCoordinates = this.geocode(endLocation, function(result) {
+        loadSuccess(mapObject, result)
       });
-    } else {
-      alert("We have not found your address. Try again.")
     }
-  }
 
-  function routingSuccess(mapObject, result) {
-    mapObject.reset();
+    function loadSuccess(mapObject, result) {
+      if(result.Response.View.length > 0 && result.Response.View[0].Result.length > 0) {
+        var locations = result.Response.View[0].Result;
+        // TODO: handle multiple results
+        var coords = locations[0].Location.DisplayPosition;
+        console.log("Destination: " + coords.Longitude + ", " + coords.Latitude);
+        mapObject.calculateRoute(mapObject.gpsObject.getLocation(), { lat: coords.Latitude, lng: coords.Longitude }, function(result) {
+          routingSuccess(mapObject, result);
+        });
+      } else {
+        alert("We have not found your address. Try again.")
+      }
+    }
 
-    console.log("Route success");
-    console.log(result);
+    function routingSuccess(mapObject, result) {
+      mapObject.reset();
 
-    for(var i = 0; i < result.response.route.length; i++) {
-      var route = result.response.route[i];
-      var strip = new H.geo.Strip(),
+      console.log("Route success");
+      console.log(result);
+
+      for(var i = 0; i < result.response.route.length; i++) {
+        var route = result.response.route[i];
+        var strip = new H.geo.Strip(),
         routeShape = route.shape,
         polyline;
 
-      routeShape.forEach(function(point) {
-        var parts = point.split(',');
-        strip.pushLatLngAlt(parts[0], parts[1]);
-      });
+        routeShape.forEach(function(point) {
+          var parts = point.split(',');
+          strip.pushLatLngAlt(parts[0], parts[1]);
+        });
 
-      polyline = new H.map.Polyline(strip, {
-        style: {
+        polyline = new H.map.Polyline(strip, {
+          style: {
+            lineWidth: 10,
+            strokeColor: inactivePathColor
+          }
+        });
+
+        mapObject.routeShapeObjects.push(polyline);
+        mapObject.routeList.push(route);
+        mapObject.addRouteToList(mapObject, i, route);
+        mapObject.saveRoute(i, route);
+      }
+
+      mapObject.map.addObjects(mapObject.routeShapeObjects);
+      mapObject.routeSelected(0);
+    }
+
+    Map.prototype.routeSelected = function(routeId) {
+      var route = this.getRoute(routeId);
+
+      for(var i = 0; i < this.routeShapeObjects.length; i++) {
+        this.routeShapeObjects[i].setStyle({
           lineWidth: 10,
           strokeColor: inactivePathColor
-        }
-      });
+        });
+      }
 
-      mapObject.routeShapeObjects.push(polyline);
-      mapObject.routeList.push(route);
-      mapObject.addRouteToList(mapObject, i, route);
-      mapObject.saveRoute(i, route);
-    }
-
-    mapObject.map.addObjects(mapObject.routeShapeObjects);
-    mapObject.routeSelected(0);
-  }
-
-  Map.prototype.routeSelected = function(routeId) {
-    var route = this.getRoute(routeId);
-
-    for(var i = 0; i < this.routeShapeObjects.length; i++) {
-      this.routeShapeObjects[i].setStyle({
+      this.routeShapeObjects[routeId].setStyle({
         lineWidth: 10,
-        strokeColor: inactivePathColor
+        strokeColor: activePathColor
       });
+
+      this.map.setViewBounds(this.routeShapeObjects[routeId].getBounds());
     }
 
-    this.routeShapeObjects[routeId].setStyle({
-      lineWidth: 10,
-      strokeColor: activePathColor
-    });
-
-    this.map.setViewBounds(this.routeShapeObjects[routeId].getBounds());
-  }
-
-  Map.prototype.addRouteToList = function (mapObject, routeId, route) {
+    Map.prototype.addRouteToList = function (mapObject, routeId, route) {
       var routeSummary = $("<div></div>").attr("id", "route" + routeId);
       routeSummary.attr("data-id", routeId);
       routeSummary.append("<h2>" + route.waypoint[0].label + "</h2>");
@@ -124,78 +131,80 @@ Map.prototype.init = function(latitude, longitude) {
       );
 
       $("#route-list").append(routeSummary);
-  }
-
-  Map.prototype.getRoute = function(routeId) {
-    console.log("Retrieving route " + routeId);
-    return JSON.parse(localStorage.getItem("route" + routeId));
-  }
-
-  Map.prototype.saveRoute = function(routeId, route) {
-    console.log("Saving route " + routeId);
-    localStorage.setItem("route" + routeId, JSON.stringify(route));
-  }
-
-  Map.prototype.geocode = function(searchTerm, success) {
-    var geocoder = this.platform.getGeocodingService(),
-    geocodingParameters = {
-      searchText: searchTerm,
-      city: 'berlin',
-      country: 'de'
-    };
-
-    geocoder.geocode(
-      geocodingParameters,
-      success,
-      geocodeError
-    );
-  }
-
-  Map.prototype.calculateRoute = function(startLocation, stopLocation, success) {
-    //this.reset();
-
-    var router = this.platform.getRoutingService(),
-    routeRequestParams = {
-      mode: 'fastest;publicTransport',
-      representation: 'display',
-      waypoint0: startLocation.lat + "," + startLocation.lng,
-      waypoint1: stopLocation.lat + "," + stopLocation.lng,
-      routeattributes: 'waypoints,summary,shape,legs',
-      maneuverattributes: 'direction,action',
-      alternatives: 1
-    };
-
-    router.calculateRoute(
-      routeRequestParams,
-      success,
-      routeCalculationError
-    );
-  }
-
-  Map.prototype.reset = function () {
-    localStorage.clear();
-
-    $("#route-list").empty();
-
-    this.map.removeObjects(this.routeShapeObjects);
-    this.routeShapeObjects = [];
-  }
-
-  Map.prototype.startGhost = function(route, routeId) {
-    console.log("Starting ghost " + route)
-    var ghost = new Ghost(mapObject);
-    var ghostIcon = document.createElement('img');
-    ghostIcon.style.cssText = "width: 40px; height: 40px; margin-top: -20px; margin-left: -20px;";
-    ghostIcon.src = "images/ghosts/" + ghostColor[routeId%ghostColor.length] + "." + iconFormat;
-    var marker =  new H.map.DomMarker(gpsObject.getLocation(), {
-      icon: new H.map.DomIcon(ghostIcon)
-    });
-    ghost.init(route, marker);
-    ghost.start();
-  }
-
-  Map.prototype.startGhosts = function () {
-    for(var i = 0; i < this.routeShapeObjects.length; i++) {
-      this.startGhost(this.getRoute(i), i);
     }
-  }
+
+    Map.prototype.getRoute = function(routeId) {
+      console.log("Retrieving route " + routeId);
+      return JSON.parse(localStorage.getItem("route" + routeId));
+    }
+
+    Map.prototype.saveRoute = function(routeId, route) {
+      console.log("Saving route " + routeId);
+      localStorage.setItem("route" + routeId, JSON.stringify(route));
+    }
+
+    Map.prototype.geocode = function(searchTerm, success) {
+      var geocoder = this.platform.getGeocodingService(),
+      geocodingParameters = {
+        searchText: searchTerm,
+        city: 'berlin',
+        country: 'de'
+      };
+
+      geocoder.geocode(
+        geocodingParameters,
+        success,
+        geocodeError
+      );
+    }
+
+    Map.prototype.calculateRoute = function(startLocation, stopLocation, success) {
+      //this.reset();
+
+      var router = this.platform.getRoutingService(),
+      routeRequestParams = {
+        mode: 'fastest;publicTransport',
+        representation: 'display',
+        waypoint0: startLocation.lat + "," + startLocation.lng,
+        waypoint1: stopLocation.lat + "," + stopLocation.lng,
+        routeattributes: 'waypoints,summary,shape,legs',
+        maneuverattributes: 'direction,action',
+        alternatives: 1
+      };
+
+      router.calculateRoute(
+        routeRequestParams,
+        success,
+        routeCalculationError
+      );
+    }
+
+    Map.prototype.reset = function () {
+      localStorage.clear();
+      
+      $("#route-list").empty();
+
+      if(this.map !== null) {
+        this.map.removeObjects(this.routeShapeObjects);
+      }
+      this.routeShapeObjects = [];
+    }
+
+    Map.prototype.startGhost = function(route, routeId) {
+      console.log("Starting ghost " + route)
+      var ghost = new Ghost(mapObject);
+      var ghostIcon = document.createElement('img');
+      ghostIcon.style.cssText = "width: 40px; height: 40px; margin-top: -20px; margin-left: -20px;";
+      ghostIcon.src = "images/ghosts/" + ghostColor[routeId%ghostColor.length] + "." + iconFormat;
+      var marker =  new H.map.DomMarker(gpsObject.getLocation(), {
+        icon: new H.map.DomIcon(ghostIcon)
+      });
+      ghost.init(route, marker);
+      ghost.start();
+    }
+
+    Map.prototype.startGhosts = function () {
+      for(var i = 0; i < this.routeShapeObjects.length; i++) {
+        this.startGhost(this.getRoute(i), i);
+      }
+    }
